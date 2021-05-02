2a423737e271fdb5b7c407200fd2dcbf2ab01512
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-05-02 07:45:13 -0300

    app redone

--- to-do-list-client/src/components/Home.js
+++ to-do-list-client/src/components/Home.js
@@ -2,14 +2,19 @@ import "../styles/Home.css"
 import List from "../components/List"
 
 const Home = (props) => {
-    
-    const data = props.data
-    console.log("props from Home component props:", props)
-    console.log("props from Home component data:", data)
-    return (
+    const data = null
+    // const data = props.data
+    // console.log("props from Home component props:", props)
+    // console.log("props from Home component data:", data)
+    // return (
+    //     <section className="section">
+    //         <h2>To do List</h2>
+    //         <List data={data} />
+    //     </section>
+    // )
+    return(
         <section className="section">
-            <h2>To do List</h2>
-            <List data={data} />
+            {!data ? <h2>Please login to see your to do list</h2> : <h2>Map data here {data}</h2>}
         </section>
     )
 }
